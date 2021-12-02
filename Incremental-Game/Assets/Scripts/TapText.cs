ee614f8c94c2a04c72dbb684408115aed82f7b0f
THORtato <37824109+THORtato@users.noreply.github.com>
2021-12-02 16:35:33 +0700

    Chapter 6

---
+++ Incremental-Game/Assets/Scripts/TapText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapText : MonoBehaviour
+{
+    public float SpawnTime = .5f;
+    public Text Text;
+
+    private float _SpawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _SpawnTime = SpawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _SpawnTime -= Time.unscaledDeltaTime;
+        if(_SpawnTime <= 0f)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Text.CrossFadeAlpha(0f, .5f, false);
+            if(Text.color.a == 0f)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
